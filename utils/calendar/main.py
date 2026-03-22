c8513c3aa1065c1a6315256d9e1f7773b9e13dee
ilya-t <extraletters@gmail.com>
2026-03-22 19:34:34 +0300

    remove excessive timezone shifting

--- utils/calendar/main.py
+++ utils/calendar/main.py
@@ -23,12 +23,10 @@ def to_reminders(reminders_file: str, reminders: []) -> {}:
     today_start = datetime.datetime(now.year, now.month, now.day)
     
     local_time = datetime.datetime.now().astimezone()
-    offset_minutes = int(local_time.utcoffset().total_seconds() / 60)
 
     end_of_day = int(datetime.datetime(now.year, now.month, now.day, 23, 59, 59).timestamp())
-    # shifting to time zone would place event at the end of current day locally
     # shifting time by default duration would place event right to end of day and not pass to second day
-    outdated_timestamp = end_of_day - (offset_minutes * 60) - (DEFAULT_DURATION_MINUTES * 60)
+    outdated_timestamp = end_of_day - (DEFAULT_DURATION_MINUTES * 60)
 
     prefix = f'{filename}: '
 
@@ -51,7 +49,6 @@ def to_reminders(reminders_file: str, reminders: []) -> {}:
             'summary':  f'{GENERATED_DESC}\nuid: {uid}', 
             'timestamp': timestamp, 
             'duration_minutes': DEFAULT_DURATION_MINUTES, 
-            'tz_offset_minutes': offset_minutes,
             'add_notification': not is_outdated,
         }
     return results
@@ -89,7 +86,6 @@ def update_reminders_at_google_calendar(app, reminders: {}):
           summary=r['summary'],
           timestamp=r['timestamp'],
           duration_minutes=r['duration_minutes'],
-          tz_offset_minutes=r['tz_offset_minutes'],
           add_notification=r['add_notification'],
         )
 
@@ -105,9 +101,8 @@ def generate_ics(reminders: dict):
 
     output_file = os.path.join(PYTHON_SCRIPT_PATH, ICS_FILENAME)
 
-    def format_dt(ts: int, tz_offset_minutes: int) -> str:
-        tz = datetime.timezone(datetime.timedelta(minutes=tz_offset_minutes))
-        dt = datetime.datetime.fromtimestamp(ts, tz)
+    def format_dt(ts: int) -> str:
+        dt = datetime.datetime.utcfromtimestamp(ts)
         return dt.strftime("%Y%m%dT%H%M%S")
 
     lines = [
@@ -119,13 +114,12 @@ def generate_ics(reminders: dict):
     for uid, r in reminders.items():
         start_ts = r["timestamp"]
         duration = r["duration_minutes"]
-        tz_offset = r["tz_offset_minutes"]
 
         end_ts = start_ts + duration * 60
 
-        dtstart = format_dt(start_ts, tz_offset)
-        dtend = format_dt(end_ts, tz_offset)
-        dtstamp = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+        dtstart = format_dt(start_ts)
+        dtend = format_dt(end_ts)
+        dtstamp = start_ts
 
         lines.extend([
             "BEGIN:VEVENT",
